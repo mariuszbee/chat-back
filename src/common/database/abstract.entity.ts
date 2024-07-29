81095d0151b723235d41b050204545f4aca62c7d
mariuszbee <bemariuszdev@gmail.com>
2024-07-29 19:56:16 +0200

    aply abstract entity to user entity

--- src/common/database/abstract.entity.ts
+++ src/common/database/abstract.entity.ts
@@ -2,7 +2,7 @@ import { Prop, Schema } from '@nestjs/mongoose';
 import { SchemaTypes, Types } from 'mongoose';
 
 @Schema()
-export class AbstractDocument {
+export class AbstractEntity {
   @Prop({ ype: SchemaTypes.ObjectId })
   _id: Types.ObjectId;
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,13 +3,11 @@ import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
 import { UsersRepository } from './users.repository';
 import { DatabaseModule } from 'src/common/database/database.module';
-import { UserDocument, UserSchema } from './entities/user.schema';
+import { User, UserSchema } from './entities/user.entity';
 
 @Module({
   imports: [
-    DatabaseModule.forFeature([
-      { name: UserDocument.name, schema: UserSchema },
-    ]),
+    DatabaseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   providers: [UsersResolver, UsersService, UsersRepository],
 })

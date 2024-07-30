ee226f9cd6662063e09dbf82eef5d999c7292248
mariuszbee <bemariuszdev@gmail.com>
2024-07-30 23:25:53 +0200

    add types to passport-local and add veryfiyUser method to user service

---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      usernameField: 'email',
+    });
+  }
+
+  async validate(email: string, password: string) {
+    // Find user in database
+    // Check if password is correct
+    // Return user
+  }
+}

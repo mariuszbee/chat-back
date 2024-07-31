cec0b139fde75dcd7c2fdc69ddefefcb44c2025b
mariuszbee <bemariuszdev@gmail.com>
2024-07-31 12:19:35 +0200

    aply auth guard in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Controller, Post, Res, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { CurrentUser } from './current-user.decorator';
+import { User } from 'src/users/entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
   @Post('login')
   @UseGuards(LocalAuthGuard)
-  async login() {}
+  async login(
+    @CurrentUser() user: User,
+    @Res({ passthrough: true }) res: Response,
+  ) {}
 }

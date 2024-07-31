ccd40751d00ae30966787386d6cffa96cc716140
mariuszbee <bemariuszdev@gmail.com>
2024-07-31 12:51:40 +0200

    aply jwt service in aut service for login method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,13 +3,18 @@ import { Response } from 'express';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { CurrentUser } from './current-user.decorator';
 import { User } from 'src/users/entities/user.entity';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Post('login')
   @UseGuards(LocalAuthGuard)
   async login(
     @CurrentUser() user: User,
     @Res({ passthrough: true }) res: Response,
-  ) {}
+  ) {
+    return this.authService.login(user, res);
+  }
 }

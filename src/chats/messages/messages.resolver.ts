efc9dbe37add23b2e4c6a735e8c28f181e194cbb
mariuszbee <bemariuszdev@gmail.com>
2024-08-06 15:32:26 +0200

    dreate subscriptions ws for create message in message resolver

--- src/chats/messages/messages.resolver.ts
+++ src/chats/messages/messages.resolver.ts
@@ -1,16 +1,22 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
 import { MessagesService } from './messages.service';
 import { Message } from './entities/message.entity';
-import { UseGuards } from '@nestjs/common';
+import { Inject, UseGuards } from '@nestjs/common';
 import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
 import { CreateMessageInput } from './dto/create-message.inpot';
 import { CurrentUser } from 'src/auth/current-user.decorator';
 import { TokenPayload } from 'src/auth/token-payload.interface';
 import { GetMessagesArgs } from './dto/get-messages.args';
+import { PUB_SUB } from 'src/common/constants/injection-token';
+import { PubSub } from 'graphql-subscriptions';
+import { MESSAGE_CREATED } from './constants/bubsub-triggers';
 
 @Resolver(() => Message)
 export class MessagesResolver {
-  constructor(private readonly messagesService: MessagesService) {}
+  constructor(
+    private readonly messagesService: MessagesService,
+    @Inject(PUB_SUB) private readonly pubSub: PubSub,
+  ) {}
 
   @Mutation(() => Message)
   @UseGuards(GqlAuthGuard)
@@ -28,5 +34,10 @@ export class MessagesResolver {
     @CurrentUser() user: TokenPayload,
   ) {
     return this.messagesService.getMessages(getMessageArgs, user._id);
+  }
+
+  @Subscription(() => Message)
+  messageCreated() {
+    return this.pubSub.asyncIterator(MESSAGE_CREATED);
   }
 }
